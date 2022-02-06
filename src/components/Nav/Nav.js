c66b1a983b21ecb5cb5e62cfe7e0d49023e7234f
unknown <amery.bur410@gmail.com>
2022-02-05 22:03:58 -0600

    finishing navbar mobile responsiveness and adding social media links.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../../App.css";
 import "./Nav.css";
 import { Nav, Navbar, Container, Button, Modal } from "react-bootstrap";
 import "bootstrap/js/src/collapse.js";
+import github from "../../images/icons/GitHub-Mark.png";
+import linkedin from "../../images/icons/LI-In-Bug.png";
+import instagram from "../../images/icons/instagram.png";
+import twitter from "../../images/icons/logo-round-twitter.png";
+import gmail from "../../images/icons/gmail.png";
+import phone from "../../images/icons/phone.png";
 function NavBar() {
   const [show, setShow] = useState(false);
 
@@ -41,21 +48,53 @@ function NavBar() {
               <span className="nav-links">Contact Me</span>
             </Nav.Link>
             <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                Woohoo, you're reading this text in a modal!
+              <Modal.Body className="modal-body">
+                <Modal.Title className="modal-title">
+                  Contact Information
+                </Modal.Title>
+                <ul className="modal-links">
+                  <li className="links-list">
+                    <a className="links" href="https://github.com/dannfirefight322">
+                      <img className="icon" src={github} alt="..." />
+                      -Github
+                    </a>
+                  </li>
+                  <li className="links-list">
+                    <a className="links" href="https://www.linkedin.com/in/daniel-burford-209b5b20a/">
+                      <img className="icon" src={linkedin} alt="..." />
+                      -Linkedin
+                    </a>
+                  </li>
+                  <li className="links-list">
+                    <a className="links" href="https://www.instagram.com/texas.photography94/">
+                      <img className="icon" src={instagram} alt="..." />
+                      -Instagram
+                    </a>
+                  </li>
+                  <li className="links-list">
+                    <a className="links" href="https://twitter.com/texasphotograp7">
+                      <img className="icon" src={twitter} alt="..." />
+                      -Twitter
+                    </a>
+                  </li>
+                  <li className="links-list">
+                    <a className="links" href="#">
+                      <img className="icon" src={gmail} alt="..." />
+                      -amery.bur410@gmail
+                    </a>
+                  </li>
+                  <li className="links-list">
+                    <a className="links" href="#">
+                      <img className="icon" src={phone} alt="..." />
+                      -210-823-3196
+                    </a>
+                  </li>
+                </ul>
               </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+                <Button className="modal-button" variant="secondary" onClick={handleClose}>
                   Close
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
-                  Save Changes
-                </Button>
-              </Modal.Footer>
-            </Modal>
+              </Modal>
           </Nav>
         </Navbar.Collapse>
       </Container>
