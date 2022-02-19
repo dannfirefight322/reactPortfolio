ef2e673aeaf3d463abba888695e7978593b91ac0
unknown <amery.bur410@gmail.com>
2022-02-19 03:51:08 -0600

    changing a few styles.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -19,115 +19,116 @@ function NavBar() {
   const handleShow = () => setShow(true);
   return (
     <div>
-    <Navbar expand="lg">
-      <Container style={{ width: "90%"}}>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <div className="toggle-container">
-            <span className="navbar-toggle-icon">|||</span>
-            <span className="toggle-title">Navigator</span>
-          </div>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">
-              <span className="nav-links">Home</span>
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/about">
-              <span className="nav-links">About Me</span>
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/projects">
-              <span className="nav-links">Projects</span>
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/certifications">
-              <span className="nav-links">Certifications</span>
-            </Nav.Link>
-            <h1>
-              <a
-                className="resumelink"
-                href="https://daniel-burford.com/Daniel_Burford.pdf"
-                target="_blank"
-              >
-                Resume
-              </a>
-            </h1>
-            <Nav.Link variant="primary" onClick={handleShow}>
-              <span className="nav-links">Contact Me</span>
-            </Nav.Link>
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Body className="modal-body">
-                <Modal.Title
-                  className="modal-title"
-                  style={{ fontSize: "35px" }}
+      <Navbar expand="lg">
+        <Container style={{ width: "90%" }}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <div className="toggle-container">
+              <span className="navbar-toggle-icon">|||</span>
+              <span className="toggle-title">Navigator</span>
+            </div>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/">
+                <span className="nav-links">Home</span>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/about">
+                <span className="nav-links">About Me</span>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/projects">
+                <span className="nav-links">Projects</span>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/certifications">
+                <span className="nav-links">Certifications</span>
+              </Nav.Link>
+              <h1>
+                <a
+                  className="resumelink"
+                  href="https://daniel-burford.com/Daniel_Burford.pdf"
+                  target="_blank"
                 >
-                  Contact Information
-                </Modal.Title>
-                <ul className="modal-links">
-                  <li className="links-list">
-                    <a
-                      className="links"
-                      href="https://github.com/dannfirefight322"
-                      target="_blank"
-                    >
-                      <img className="icon" src={github} alt="..." />- Github
-                    </a>
-                  </li>
-                  <li className="links-list">
-                    <a
-                      className="links"
-                      href="https://www.linkedin.com/in/daniel-burford-209b5b20a/"
-                      target="_blank"
-                    >
-                      <img className="icon" src={linkedin} alt="..." />-
-                      Linkedin
-                    </a>
-                  </li>
-                  <li className="links-list">
-                    <a
-                      className="links"
-                      href="https://www.instagram.com/texas.photography94/"
-                      target="_blank"
-                    >
-                      <img className="icon" src={instagram} alt="..." />-
-                      Instagram
-                    </a>
-                  </li>
-                  <li className="links-list">
-                    <a
-                      className="links"
-                      href="https://twitter.com/texasphotograp7"
-                      target="_blank"
-                    >
-                      <img className="icon" src={twitter} alt="..." />- Twitter
-                    </a>
-                  </li>
-                  <li className="links-list">
-                    <a className="links" href="#">
-                      <img className="icon" src={gmail} alt="..." />-
-                      amery.bur410@gmail
-                    </a>
-                  </li>
-                  <li className="links-list">
-                    <a className="links" href="#">
-                      <img className="icon" src={phone} alt="..." />-
-                      210-823-3196
-                    </a>
-                  </li>
-                </ul>
-              </Modal.Body>
-              <Button
-                className="modal-button"
-                variant="secondary"
-                onClick={handleClose}
-              >
-                Close
-              </Button>
-            </Modal>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>  
-    </Navbar>
-    <hr className="break"/>
-  </div>
+                  Resume
+                </a>
+              </h1>
+              <Nav.Link variant="primary" onClick={handleShow}>
+                <span className="nav-links">Contact Me</span>
+              </Nav.Link>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Body className="modal-body">
+                  <Modal.Title
+                    className="modal-title"
+                    style={{ fontSize: "35px" }}
+                  >
+                    Contact Information
+                  </Modal.Title>
+                  <ul className="modal-links">
+                    <li className="links-list">
+                      <a
+                        className="links"
+                        href="https://github.com/dannfirefight322"
+                        target="_blank"
+                      >
+                        <img className="icon" src={github} alt="..." />- Github
+                      </a>
+                    </li>
+                    <li className="links-list">
+                      <a
+                        className="links"
+                        href="https://www.linkedin.com/in/daniel-burford-209b5b20a/"
+                        target="_blank"
+                      >
+                        <img className="icon" src={linkedin} alt="..." />-
+                        Linkedin
+                      </a>
+                    </li>
+                    <li className="links-list">
+                      <a
+                        className="links"
+                        href="https://www.instagram.com/texas.photography94/"
+                        target="_blank"
+                      >
+                        <img className="icon" src={instagram} alt="..." />-
+                        Instagram
+                      </a>
+                    </li>
+                    <li className="links-list">
+                      <a
+                        className="links"
+                        href="https://twitter.com/texasphotograp7"
+                        target="_blank"
+                      >
+                        <img className="icon" src={twitter} alt="..." />-
+                        Twitter
+                      </a>
+                    </li>
+                    <li className="links-list">
+                      <a className="links" href="#">
+                        <img className="icon" src={gmail} alt="..." />-
+                        amery.bur410@gmail
+                      </a>
+                    </li>
+                    <li className="links-list">
+                      <a className="links" href="#">
+                        <img className="icon" src={phone} alt="..." />-
+                        210-823-3196
+                      </a>
+                    </li>
+                  </ul>
+                </Modal.Body>
+                <Button
+                  style={{ backgroundColor: "var(--mainColor", fontFamily: "bold", fontSize: "20px", color: "var(--pink)"}}
+                  variant="secondary"
+                  onClick={handleClose}
+                >
+                  Close
+                </Button>
+              </Modal>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <hr className="break" />
+    </div>
   );
 }
 
